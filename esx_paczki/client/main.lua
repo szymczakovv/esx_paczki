4f5fd22b82db0d02920282b91bf548717c5b4622
szymczakovv <67207947+szymczakovv@users.noreply.github.com>
2020-07-19 14:56:44 +0200

    Update main.lua

--- esx_paczki/client/main.lua
+++ esx_paczki/client/main.lua
@@ -5,6 +5,9 @@
 -- Wersja: 0.01
 
 local CurrentAction           = nil
+local blips = {
+     {title="Części Samochodowe", colour=5, id=478, x = -150.23, y = -1183.81, z = 24.06 }
+}
 local CurrentActionMsg        = ''
 local CurrentActionData       = {}
 local HasAlreadyEnteredMarker = false
@@ -198,4 +201,19 @@ AddEventHandler('esx_paczki:startJob', function()
 	ClearPedTasksImmediately(pP)
 	end
 	until(CanLeaveBox == false)
+end)
+
+Citizen.CreateThread(function()
+
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, 1.0)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, true)
+	  BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end
 end)
